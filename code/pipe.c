0bb6e533e1096fc7c93e2c5f13dc667f7ae92d7f
Karun Tancharoen <harukajidoto@gmail.com>
2025-08-19 22:30:53 +0700

    Modified the pipe code.

--- code/pipe.c
+++ code/pipe.c
@@ -10,19 +10,19 @@ int main() {
 
     if (pid == 0) {
         pid_t childPID = getpid();
+        char message[100];
+        snprintf(message, sizeof(message), "Hello from a child. PID %d.", (int) childPID);
         printf("Child. PID, %d.\n", (int) childPID);
         close(fd[0]);
-        char message[] = "Hello from a child.";
-        write(fd[1], &childPID, sizeof(childPID));
+        write(fd[1], message, strlen(message) + 1);
         close(fd[1]);
         _exit(0);
     } else {
         printf("Parent. PID, %d.\n", (int) getpid());
         close(fd[1]);
         char buffer[100];
-        pid_t receivedPID;
-        read(fd[0], &receivedPID, sizeof(receivedPID));
-        printf("%d\n", (int) receivedPID);
+        read(fd[0], buffer, sizeof(buffer));
+        printf("%s\n", buffer);
         close(fd[0]);
     }
 
