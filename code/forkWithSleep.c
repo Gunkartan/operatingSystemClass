28fef8983fa74b3137ebff99c1403b9b17ecc503
Karun Tancharoen <harukajidoto@gmail.com>
2025-08-19 20:45:54 +0700

    Finished the fork with sleep code.

---
+++ code/forkWithSleep.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello, world. PID, %d.\n", (int) getpid());
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "Fork failed.\n");
+        exit(1);
+    } else if (rc == 0) {
+        sleep(1);
+        printf("Hello, I am a child. PID, %d.\n", (int) getpid());
+    } else {
+        sleep(5);
+        printf("Hello, I am a parent of %d. PID, %d.\n", rc, (int) getpid());
+    }
+
+    return 0;
+}
