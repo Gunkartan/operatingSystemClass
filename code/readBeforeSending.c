dee6568381178f4904ea87ceb476f59a2c85beb5
Karun Tancharoen <harukajidoto@gmail.com>
2025-08-19 23:29:11 +0700

    Finished the read before sending code.

---
+++ code/readBeforeSending.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    int fd[2];
+    pipe(fd);
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        pid_t childPID = getpid();
+        char message[100];
+        snprintf(message, sizeof(message), "Hello from a child. PID, %d.", (int) childPID);
+        printf("Child. PID, %d.\n", (int) childPID);
+        sleep(3);
+        close(fd[0]);
+        write(fd[1], message, strlen(message) + 1);
+        close(fd[1]);
+        _exit(0);
+    } else {
+        printf("Parent. PID, %d.\n", (int) getpid());
+        close(fd[1]);
+        char buffer[100];
+        read(fd[0], buffer, sizeof(buffer));
+        printf("%s\n", buffer);
+        close(fd[0]);
+    }
+
+    return 0;
+}
