fa7e65d09452ad708ea8eb390d52e258923f1152
Karun Tancharoen <harukajidoto@gmail.com>
2025-09-30 16:22:23 +0700

    Modified the race condition code to use a single thread.

--- code/fifthLab/race.c
+++ code/fifthLab/race.c
@@ -14,9 +14,9 @@ void *increment(void *arg) {
 int main() {
     pthread_t first, second;
     pthread_create(&first, NULL, increment, NULL);
-    pthread_create(&second, NULL, increment, NULL);
+    // pthread_create(&second, NULL, increment, NULL);
     pthread_join(first, NULL);
-    pthread_join(second, NULL);
+    // pthread_join(second, NULL);
     printf("%d\n", counter);
 
     return 0;
