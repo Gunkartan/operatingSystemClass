cdc5ee570853455b619c17b1cf9bf4479505ca94
Karun Tancharoen <harukajidoto@gmail.com>
2025-09-30 16:29:02 +0700

    Increased the number of loops.

--- code/fifthLab/race.c
+++ code/fifthLab/race.c
@@ -4,7 +4,7 @@
 int counter = 0;
 
 void *increment(void *arg) {
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < 100000; i++) {
         counter += 2;
     }
 
@@ -14,9 +14,9 @@ void *increment(void *arg) {
 int main() {
     pthread_t first, second;
     pthread_create(&first, NULL, increment, NULL);
-    // pthread_create(&second, NULL, increment, NULL);
+    pthread_create(&second, NULL, increment, NULL); //Comment this line to test the validity of the code using a single thread.
     pthread_join(first, NULL);
-    // pthread_join(second, NULL);
+    pthread_join(second, NULL); //Comment this line to test the validity of the code using a single thread.
     printf("%d\n", counter);
 
     return 0;
